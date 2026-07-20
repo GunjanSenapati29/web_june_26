3f0e0be5799470f16ebe4879dbb80afbeb7a565c
GunjanSenapati29 <gunjandisha2005@gmail.com>
2026-07-20 09:24:16 +0530

    update.

---
+++ JavaScript/Lec 01/diff betn var,let & const.js
@@ -0,0 +1,47 @@
+function myFun() {
+  let var1 = 10;
+  const var2 = 20;
+  var var3 = 30
+}
+
+myFun()
+
+// console.log(var1)
+// console.log(var2)
+// console.log(var3)
+
+// {
+//     let a = 10;
+//     const b = 20;
+//     var c = 30
+// }
+
+// function fun() {
+//     debugger
+//     let a1 = 10;
+//     const b1 = 20;
+//     var c1 = 30;
+// }
+
+// fun()
+
+// ii:
+// const name = "Gunjan"
+// console.log("Before reassign", name)
+// name = 'Senapati'
+// console.log('After reassign', name)
+
+// iii:
+// var instituteName = "JT"
+// console.log("instituteName--------", instituteName)
+// var instituteName = "Java Technocart"
+// console.log("instituteName========", instituteName)
+
+// iv:
+debugger
+console.log(x)
+console.log(y)
+
+
+var x = 10
+let y = 20;
