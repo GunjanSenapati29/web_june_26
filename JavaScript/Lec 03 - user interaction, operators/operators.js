135daf8dd3b5c8148158c262ddee40dfbcd6ef30
GunjanSenapati29 <gunjandisha2005@gmail.com>
2026-07-24 07:38:13 +0530

    truthy and falsey

---
+++ JavaScript/Lec 03 - user interaction, operators/operators.js
@@ -0,0 +1,33 @@
+// console.log(2 ** 3)
+// console.log(4 ** 3)
+
+// let x = 10
+// x += 10
+// console.log(x) 
+
+// x **= 2
+// console.log(x) 
+
+// console.log(5 == "5")
+// console.log(1 == true)
+// console.log(0 == false)
+// console.log(5 == "4")
+// console.log(5 == "")
+
+
+// console.log(5 === "5")
+// console.log(1 === true)
+// console.log(0 === false)
+
+
+// 4 * 2^3
+console.log(2 << 3) 
+console.log(8 << 5) // 8 * 2^5 // 2
+
+console.log(64 >> 5) // 64 / 2^5 // 2
+console.log(256 >> 3) // 256 / 2^3 // 
+
+let a = 10
+let b = 15
+
+console.log( a > b ? a : b)
---
+++ JavaScript/Lec 03 - user interaction, operators/user interaction.js
@@ -0,0 +1,20 @@
+// alert("Hello Everyone!")
+// window.alert("Good Morning")
+
+// let name = prompt("Enter Your Name:");
+
+// if (name == "") {
+//     console.log("name is", prompt("Re enter user name "))
+// } else {
+//     console.log("Your name is", name);
+// }
+
+
+
+// let choice = confirm("Are you enjoying js classes ?")
+// console.log(choice)
+
+
+console.log("Hii") // normal
+console.warn("Hello") // yellow 
+console.error("Bye") // Red
