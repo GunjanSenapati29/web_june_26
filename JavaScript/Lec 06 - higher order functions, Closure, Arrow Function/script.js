0ab58af0e708a19e5ba13c442e61d8c982e9b6bc
GunjanSenapati29 <gunjandisha2005@gmail.com>
2026-07-29 08:52:18 +0530

    Higher Order Functions.

---
+++ JavaScript/Lec 06 - higher order functions, Closure, Arrow Function/script.js
@@ -0,0 +1,55 @@
+// function fun(f1){
+//     console.log("Value of 'f1' is: ",f1)
+// }
+
+// Normal Function
+// fun(10)
+// fun(true)
+// fun("JT")
+
+// Higher Order Function
+// fun(hello())
+// fun(hello)
+
+// function hello(){
+//     console.log("Hello Everyone!")
+//     return 100
+// }
+
+
+// ====================
+// function f3(){
+//     console.log("This is f3 function")
+// }
+
+// function f1(){
+//     console.log("This is f1 function")
+//     // return 101
+
+//     return function f2(){
+//         console.log('This is f2 function')
+//     }
+    // return f3
+
+// }
+
+// let x = f1()
+// console.log("/////////", x)
+// x()
+
+// ============= setTimeout, setInterval ============
+// console.log("Hello")
+// setTimeout(function f1() {
+//     console.log("Have a nice Day!")
+// }, 5000)
+// console.log("Gunjan")
+
+function f1(){
+    console.log('Hiiiiiiiii')
+}
+const intervalid = setInterval(f1, 2000)
+console.log("//////////", intervalid)
+setTimeout(function f2(){
+    console.log("Clearing Interval")
+    clearInterval(intervalid)
+}, 10000)
