28e88f15d51dc9fbc673e6fd1a2013b04c12e35f
GunjanSenapati29 <gunjandisha2005@gmail.com>
2026-08-11 08:27:17 +0530

    BOM Javascript

---
+++ JavaScript/Lec 12 - Strings/template literal .js
@@ -0,0 +1,8 @@
+const a = 10
+const b = 20
+
+const str = 'the value of a is '+a+" the value of b is "+b+" sum of a & b is "+(a+b)
+console.log(str) 
+
+const templateString = `Using template literal = a value is ${a}, b value is ${b}, sum of a & b is ${a+b}`
+console.log(templateString)
